e40f3682d82565ecc9ac04e661de66ebd2537df5
qiqigou <2319002545@qq.com>
2020-01-06 15:16:49 +0800

    0

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -13,47 +13,18 @@ namespace ConsoleApp1
 {
     class Program
     {
-        public static string GetCode()
-        {
-            //var baseUrl = Directory.GetCurrentDirectory();
-            var temp = Path.Combine(@"..\..\", "模板语法.txt");
-            var data = Path.Combine(@"..\..\", "模板数据.json");
-
-            var manager = new TempManager();
-            manager.LoadFromFile(temp);
-            var build = manager.GetTempBuild();
-
-            build.LoadFromFile(data);
-            var code = build.Execute();
-
-            //var dlls = new string[] {
-            //    "System.ComponentModel.DataAnnotations.dll",
-            //    "ConsoleApp1.exe"
-            //};
-
-            return code;
-        }
-
         static void Main(string[] args)
         {
-            //Assembly = GetAssembly();
-            //Type type = Assembly.GetType("ConsoleApp1.Users");
+            var tempPath = Path.Combine(@"..\..\", "模板语法.txt");
+            var dataPath = Path.Combine(@"..\..\", "模板数据.json");
 
-            //var helper = new DBHelper();
-            //using (var db = new DALContext())
-            //{
-            //    //var list = helper.Filter(db, type, "userid=\"100\"");
+            var build = new TempManager(tempPath).GetTempBuild();
+            build.LoadFromFile(dataPath);
+            var code = build.Execute();
 
-            //    var json = JsonConvert.SerializeObject(list, Formatting.Indented);
-            //    Console.WriteLine(json);
-            //}
-            var code = GetCode();
             Console.WriteLine(code);
             Console.Read();
         }
-
-
-
 
     }
 }
